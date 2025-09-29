ee02aaec68dacabd6be79ca0fce55e967547eae9
Efrat Friedman <efratfriedman18@gmail.com>
2025-09-29 13:35:02 +0300

    Add fetchProducts function to retrieve product data

---
+++ src/service/products.js
@@ -0,0 +1,10 @@
+import { httpGet } from "./http";
+
+export const fetchProducts = () => {
+    return httpGet(
+        '/products',
+        {}
+    )
+        .then(data => { console.log('Products :', data); return data; })
+        .catch((error) => { console.error('== Error fetching products :', error); throw error; })
+};
